481c672211937d860a9afb4a437d17e292ca5f46
chadtoolio <121692418+chadtoolio@users.noreply.github.com>
2023-01-04 18:20:52 -0500

    initial commit

---
+++ emp_datab.py
@@ -0,0 +1,4 @@
+# employee database portal office access only
+    # TODO input from gui can add employees with password
+    # TODO password true false statements added for next step
+    # TODO a standard practice that can be done by any manager.
