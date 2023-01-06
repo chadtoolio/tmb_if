f52437f64893c038f4d9cfd31c0cd5ac300ee4c0
totalminibmw <totalminibmw.chad@att.net>
2023-01-06 16:59:20 -0500

    Changed name of emp database excel file to test_db.  Added random data in  additional spread sheets for testing purposes

--- emp_datab.py
+++ emp_datab.py
@@ -3,3 +3,9 @@
     # TODO password true false statements added for next step
     # TODO a standard practice that can be done by any manager.
 
+from openpyxl import load_workbook
+wb = load_workbook('test_db.xlsx')
+wb = wb2["test_db.xlsx"]
+print(wb2.sheetnames)
+['Sheet2', 'New Title', 'Sheet1']
+
