cb4157fe9d6c8bd15b171d727b67626be84566c7
totalminibmw <totalminibmw.chad@att.net>
2023-01-06 11:23:55 -0500

    Added Openpyxl to packages, trying to import worksheet on emp_datab.py

--- emp_datab.py
+++ emp_datab.py
@@ -3,7 +3,9 @@
     # TODO password true false statements added for next step
     # TODO a standard practice that can be done by any manager.
 
+import openpyxl
 from openpyxl import Workbook
+wb =
 
 class emp_data:
     def __int__(self, employee, doh, pay_scale, dept ):
