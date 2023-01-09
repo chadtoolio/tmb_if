444b2c40e09cf96d10b6b7d0fccda3fe2d1d6515
totalminibmw <totalminibmw.chad@att.net>
2023-01-09 09:04:00 -0500

    Removed upper comment to emp_datab.py that no longer applied

--- emp_datab.py
+++ emp_datab.py
@@ -1,4 +1,3 @@
-# employee database portal office access only
     # TODO input from gui can add employees with password
     # TODO password true false statements added for next step
     # TODO a standard practice that can be done by any manager.
